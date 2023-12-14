8de81da8cf84ae6941198ed2e3d9d40a15603e3b
Dark Chicken <108430933+duythi3n@users.noreply.github.com>
2023-12-14 10:53:55 +0700

    baocao
    sghaugioey

---
+++ demo_sweetsoft/js/scrip.js
@@ -0,0 +1,47 @@
+const lists = document.querySelector('.slider .list');
+const items = document.querySelectorAll('.slider .list .itemt');
+const nuts = document.querySelectorAll('.slider .nuts li');
+
+let active = 0;
+
+const updateSlider = () => {
+    const checkLeft = items[active].offsetLeft;
+    lists.style.left = -checkLeft + 'px';
+    updateNuts();
+};
+
+const updateNuts = () => {
+    nuts.forEach((nut, index) => {
+        nut.classList.toggle('active', index === active);
+    });
+};
+
+const nextSlide = () => {
+    active = (active + 1) % items.length;
+    updateSlider();
+};
+
+const prevSlide = () => {
+    active = (active - 1 + items.length) % items.length;
+    updateSlider();
+};
+
+
+document.querySelector('.nuts').addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+        active = Array.from(nuts).indexOf(event.target);
+        updateSlider();
+    }
+});
+setInterval(nextSlide, 3000);
+
+//product
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.product-itemt');
+    document.getElementById('product-items').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.product-itemt');
+    document.getElementById('product-items').prepend(lists[lists.length - 1]);
+}
+
---
+++ demo_sweetsoft/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    './**/*.html',
+    './**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
